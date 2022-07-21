f54f2ef1c59bb83216c91c18f125684d415044e3
Lawen-s <bg920611@gmail.com>
2022-07-21 13:37:29 +0900

    feat: url 학습

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = 'blog' # URL Reverse에서 namespace 역할을 하게 된다.
+
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,24 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+
+
+class YearConberter:
+    regex = r"20\d{2}"
+    
+    def to_python(self, value):
+        return int(value)
+    
+    def to_url(self, value):
+        return str(value)
+
+register_converter(YearConberter, 'year')
+
+app_name = 'instagram' # URL Reverse에서 namespace 역할을 하게 된다.
 
 urlpatterns = [
     path('', views.post_list),
-    path('<int:pk>', views.post_detail)
+    path('<int:pk>', views.post_detail),
+    # path('archives/<int:year>/', views.archives_year),
+    # re_path(r'archives/(?P<year>\d{4})/', views.archives_year), # url을 새로 만들어놓은 경우
+    path('archives/<year:year>/', views.archives_year),
 ]
